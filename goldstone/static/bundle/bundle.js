d67afcf94ecd10452e8e84190b5fe0b1cda09df0
Alex Jacobs <alex@solinea.com>
2015-05-18 23:39:14 -0700

    Adds multiple lookback options up to 1 month.

--- goldstone/static/bundle/bundle.js
+++ goldstone/static/bundle/bundle.js
@@ -4645,10 +4645,15 @@ var GlobalLookbackRefreshButtonsView = Backbone.View.extend({
             });
             return result;
         } else {
-            return '<option value="15">lookback 15m</option>' +
-                '<option value="60" selected>lookback 1h</option>' +
+            return '<option value="15" selected>lookback 15m</option>' +
+                '<option value="60">lookback 1h</option>' +
                 '<option value="360">lookback 6h</option>' +
-                '<option value="1440">lookback 1d</option>';
+                '<option value="1440">lookback 1d</option>' +
+                '<option value="5760">lookback 3d</option>' +
+                '<option value="7200">lookback 5d</option>' +
+                '<option value="10080">lookback 1w</option>' +
+                '<option value="20160">lookback 2w</option>' +
+                '<option value="43200">lookback 4w</option>';
         }
     },
 
@@ -4664,10 +4669,10 @@ var GlobalLookbackRefreshButtonsView = Backbone.View.extend({
             });
             return result;
         } else {
-            return '<option value="30" selected>refresh 30s</option>' +
+            return '<option value="30">refresh 30s</option>' +
                 '<option value="60">refresh 1m</option>' +
                 '<option value="300">refresh 5m</option>' +
-                '<option value="-1">refresh off</option>';
+                '<option value="-1" selected>refresh off</option>';
         }
     },
 
